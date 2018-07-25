997024beb5bfe1aac7a65c37a0a77dc29cd1bb6f
zhyang <yangzhen2014@ia.ac.cn>
2018-07-25 14:41:27 +0800

    update the vocab.py

--- vocab.py
+++ vocab.py
@@ -1,45 +1,46 @@
 # -*- coding: utf-8 -*-
 #/usr/bin/python2
-"""
-June 2017 by kyubyong park. 
-kbpark.linguist@gmail.com.
-https://www.github.com/kyubyong/transformer
-Modified by Chunqi Wang in July 2017.
-"""
-from __future__ import print_function
-import codecs
-import re as regex
-import yaml
-from argparse import ArgumentParser
-from collections import Counter
 
-from utils import AttrDict
+import numpy
+import cPickle as pkl
+#import ipdb
+import sys
+import fileinput
 
+from collections import OrderedDict
 
-def make_vocab(fpath, fname):
-    """Constructs vocabulary.
-    
-    Args:
-      fpath: A string. Input file path.
-      fname: A string. Output file name.
-    
-    Writes vocabulary line by line to `fname`.
-    """
-    word2cnt = Counter()
-    for l in codecs.open(fpath, 'r', 'utf-8'):
-        words = l.split()
-        word2cnt.update(Counter(words))
-    with codecs.open(fname, 'w', 'utf-8') as fout:
-        fout.write("{}\t1000000000\n{}\t1000000000\n{}\t1000000000\n{}\t1000000000\n".format("<PAD>", "<UNK>", "<S>", "</S>"))
-        for word, cnt in word2cnt.most_common(len(word2cnt)):
-            fout.write(u"{}\t{}\n".format(word, cnt))
+def main():
+    for filename in sys.argv[1:]:
+        print 'Processing', filename
+        word_freqs = OrderedDict()
+        with open(filename, 'r') as f:
+            for line in f:
+                words_in = line.strip().split(' ')
+                for w in words_in:
+                    if w not in word_freqs:
+                        word_freqs[w] = 0
+                    word_freqs[w] += 1
+        words = word_freqs.keys()
+        freqs = word_freqs.values()
+        #print freqs
+        #ipdb.set_trace()
+        sorted_idx = numpy.argsort(freqs)
+        #print sorted_idx
+        sorted_words = [words[ii] for ii in sorted_idx[::-1]]
+
+        worddict = OrderedDict()
+        worddict['<PAD>'] = 0
+        worddict['<UNK>'] = 1
+        worddict['<S>'] = 1
+        worddict['</S>'] = 1
+        for ii, ww in enumerate(sorted_words):
+            worddict[ww] = ii+4
+
+        with open('%s.voc'%filename, 'wb') as f:
+            for key, _ in worddict.items():
+                f.write(key +'\n')
+
+        print 'Done'
 
 if __name__ == '__main__':
-    parser = ArgumentParser()
-    parser.add_argument('-c', '--config', dest='config')
-    args = parser.parse_args()
-    # Read config
-    config = AttrDict(yaml.load(open(args.config)))
-    make_vocab(config.train.src_path, config.src_vocab)
-    make_vocab(config.train.dst_path, config.dst_vocab)
-    print("Done")
+    main()
